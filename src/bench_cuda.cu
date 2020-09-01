7fcfe9b8a5626dcbd4fb73c243353ce4b1649d21
Thomas Petig <thomas@petig.eu>
2020-09-01 11:38:15 +0200

    updated test

--- src/bench_cuda.cu
+++ src/bench_cuda.cu
@@ -6,43 +6,100 @@ using namespace std;
 __global__
 void add(int n, float *x, float *y)
 {
-  for (int i = 0; i < n; i++)
-    y[i] = x[i] + y[i];
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+	  y[i] = x[i] + y[i];
+}
+
+__global__
+void mul(int n, float *x, float *y)
+{
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+    y[i] = x[i] * y[i];
 }
 
 int main(void)
 {
-  int N = 1<<20;
+  int N = 100;
   float *x, *y;
+  int blockSize = 256;
+  int numBlocks = (N + blockSize - 1) / blockSize;
 
   auto start = std::chrono::system_clock::now ();
   // Allocate Unified Memory – accessible from CPU or GPU
   cudaMallocManaged(&x, N*sizeof(float));
   cudaMallocManaged(&y, N*sizeof(float));
 
+  auto stop = std::chrono::system_clock::now ();
+  chrono::duration< double > dur = stop - start;
+  std::cout << "alloc took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
   // initialize x and y arrays on the host
   for (int i = 0; i < N; i++) {
     x[i] = 1.0f;
     y[i] = 2.0f;
   }
-  auto stop = std::chrono::system_clock::now ();
-  chrono::duration< double > dur = stop - start;
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
   std::cout << "init took " << dur.count () << " s " << std::endl;
 
+  auto tstart = std::chrono::system_clock::now ();
   start = std::chrono::system_clock::now ();
   // Run kernel on 1M elements on the GPU
-  add<<<1, 1>>>(N, x, y);
+  add<<<numBlocks, blockSize>>>(N, x, y);
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "add took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
+  // Run kernel on 1M elements on the GPU
+  add<<<numBlocks, blockSize>>>(N, x, y);
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "add took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
+  // Run kernel on 1M elements on the GPU
+  add<<<numBlocks, blockSize>>>(N, x, y);
   stop = std::chrono::system_clock::now ();
   dur = stop - start;
   std::cout << "add took " << dur.count () << " s " << std::endl;
 
   start = std::chrono::system_clock::now ();
+  // Run kernel on 1M elements on the GPU
+  add<<<numBlocks, blockSize>>>(N, x, y);
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "add took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
+  // Run kernel on 1M elements on the GPU
+  mul<<<numBlocks, blockSize>>>(N, x, y);
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "mul took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
+  // Run kernel on 1M elements on the GPU
+  mul<<<numBlocks, blockSize>>>(N, x, y);
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "mul took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
+  // Run kernel on 1M elements on the GPU
+  mul<<<numBlocks, blockSize>>>(N, x, y);
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "mul took " << dur.count () << " s " << std::endl;
+
+  start = std::chrono::system_clock::now ();
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
-
   stop = std::chrono::system_clock::now ();
   dur = stop - start;
   std::cout << "sync took " << dur.count () << " s " << std::endl;
+  auto tstop = std::chrono::system_clock::now ();
+  dur = tstop - tstart;
+  std::cout << "total took " << dur.count () << " s " << std::endl;
 
   // Check for errors (all values should be 3.0f)
   float maxError = 0.0f;
--- src/bench_eigen.cpp
+++ src/bench_eigen.cpp
@@ -9,25 +9,47 @@ int main (void)
   std::cout << "Eigen benchmark" << std::endl;
 
   auto start = std::chrono::system_clock::now ();
-  MatMul< float, 100, 1000 > mm;
+  MatMul< float, 100, 100 > mm;
+  MatMul< float, 100, 100 > madd;
   auto stop = std::chrono::system_clock::now ();
-
   chrono::duration< double > dur = stop - start;
   std::cout << "init took " << dur.count () << " s " << std::endl;
-  start = std::chrono::system_clock::now ();
-  mm.run ();
-  stop = std::chrono::system_clock::now ();
-  dur = stop - start;
-  std::cout << "multiplication took " << dur.count () << " s " << std::endl;
-  std::cout << mm.res().determinant () << std::endl;
-  MatMul< float, 500, 500 > madd;
+
+
+  auto tstart = std::chrono::system_clock::now ();
   start = std::chrono::system_clock::now ();
   madd.add ();
   stop = std::chrono::system_clock::now ();
   dur = stop - start;
   std::cout << "add took " << dur.count () << " s " << std::endl;
-  std::cout << madd.res().determinant () << std::endl;
+  start = std::chrono::system_clock::now ();
+  madd.add ();
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "add took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
+  madd.add ();
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "add took " << dur.count () << " s " << std::endl;
 
+  start = std::chrono::system_clock::now ();
+  mm.run ();
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "multiplication took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
+  mm.run ();
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "multiplication took " << dur.count () << " s " << std::endl;
+  start = std::chrono::system_clock::now ();
+  mm.run ();
+  stop = std::chrono::system_clock::now ();
+  dur = stop - start;
+  std::cout << "multiplication took " << dur.count () << " s " << std::endl;
+  dur = stop - tstart;
+  std::cout << "total took " << dur.count () << " s " << std::endl;
 
 
   return 0;
