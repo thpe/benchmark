34e0c00f7d4e4c75db24d35e8694fb49d7bc2557
Thomas Petig <thomas@petig.eu>
2021-03-29 22:02:17 +0200

    added some more interations

--- src/bench_eigen.cpp
+++ src/bench_eigen.cpp
@@ -1,6 +1,7 @@
 #define EIGEN_STACK_ALLOCATION_LIMIT 0
 #include <chrono>
 #include <iostream>
+
 #include "eigen_matmul.hpp"
 
 using namespace std;
@@ -11,43 +12,57 @@ int main (void)
   auto start = std::chrono::system_clock::now ();
   MatMul< float, 100, 100 > mm;
   MatMul< float, 100, 100 > madd;
+  MatMul< float, 200, 200 > mm1;
+  MatMul< float, 200, 200 > madd1;
   auto stop = std::chrono::system_clock::now ();
   chrono::duration< double > dur = stop - start;
   std::cout << "init took " << dur.count () << " s " << std::endl;
 
+  double dursum = 0.0;
+  int runs = 16;
 
   auto tstart = std::chrono::system_clock::now ();
-  start = std::chrono::system_clock::now ();
-  madd.add ();
-  stop = std::chrono::system_clock::now ();
-  dur = stop - start;
-  std::cout << "add took " << dur.count () << " s " << std::endl;
-  start = std::chrono::system_clock::now ();
-  madd.add ();
-  stop = std::chrono::system_clock::now ();
-  dur = stop - start;
-  std::cout << "add took " << dur.count () << " s " << std::endl;
-  start = std::chrono::system_clock::now ();
-  madd.add ();
-  stop = std::chrono::system_clock::now ();
-  dur = stop - start;
-  std::cout << "add took " << dur.count () << " s " << std::endl;
 
-  start = std::chrono::system_clock::now ();
-  mm.run ();
-  stop = std::chrono::system_clock::now ();
-  dur = stop - start;
-  std::cout << "multiplication took " << dur.count () << " s " << std::endl;
-  start = std::chrono::system_clock::now ();
-  mm.run ();
-  stop = std::chrono::system_clock::now ();
-  dur = stop - start;
-  std::cout << "multiplication took " << dur.count () << " s " << std::endl;
-  start = std::chrono::system_clock::now ();
-  mm.run ();
-  stop = std::chrono::system_clock::now ();
-  dur = stop - start;
-  std::cout << "multiplication took " << dur.count () << " s " << std::endl;
+  for (int i = 0; i < runs; i++) {
+    start = std::chrono::system_clock::now ();
+    madd.add ();
+    stop = std::chrono::system_clock::now ();
+    dur = stop - start;
+    dursum += dur.count ();
+  }
+  std::cout << "add took " << dursum / (double)runs << " s in average" << std::endl;
+
+  dursum = 0.0;
+  for (int i = 0; i < runs; i++) {
+    start = std::chrono::system_clock::now ();
+    mm.run ();
+    stop = std::chrono::system_clock::now ();
+    dur = stop - start;
+    dursum += dur.count ();
+  }
+  std::cout << "multiplication took " << dursum / (double)runs << " s in average" << std::endl;
+
+
+  dursum = 0.0;
+  for (int i = 0; i < runs; i++) {
+    start = std::chrono::system_clock::now ();
+    madd1.add ();
+    stop = std::chrono::system_clock::now ();
+    dur = stop - start;
+    dursum += dur.count ();
+  }
+  std::cout << "add took " << dursum / (double)runs << " s in average" << std::endl;
+
+  dursum = 0.0;
+  for (int i = 0; i < runs; i++) {
+    start = std::chrono::system_clock::now ();
+    mm1.run ();
+    stop = std::chrono::system_clock::now ();
+    dur = stop - start;
+    dursum += dur.count ();
+  }
+  std::cout << "multiplication took " << dursum / (double)runs << " s in average" << std::endl;
+
   dur = stop - tstart;
   std::cout << "total took " << dur.count () << " s " << std::endl;
 
